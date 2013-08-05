67cbf35e46df6297686c8f642ffb6a17dc2562c4
Ben52 <bygreenes@gmail.com>
2013-08-05 00:18:27 -0400

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,5 +1,22 @@
 <?php
-      echo 'hello world';
+      Class Foods{
+            Public $edible = yes;
+            Public $type;
+            Public $color;
+            Public $taste;
+            
+            Public function __construct($type, $color, $taste){
+                    $this->type = $type;
+                    $this->color = $color;
+                    $this->taste= $taste;
+                    }
+
+
+}
+
+$apple = new Foods;
+
+            
 
 
 
