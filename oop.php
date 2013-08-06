ada980e24fefc034ed97cf915ba01e4091bf4514
Ben52 <bygreenes@gmail.com>
2013-08-06 16:11:56 -0400

    oop cars

---
+++ oop.php
@@ -0,0 +1,25 @@
+<?php
+     class cars {
+           
+           public $doors;
+           public $color;
+           public $year;
+           public $name;
+            
+          public function  __construct($doors, $color, $year, $name){
+          $this->doors = $doors;
+          $this->color = $color;
+          $this->year = $year;
+          $this->name = $name;
+          } 
+     }
+
+
+     $kiaOptima = new cars (4, 'tan', 2004, 'Kia Optima');
+     $fordExplorer = new cars;
+     
+print_r($kiaOptima);
+print_r($fordExplorer);
+
+
+ ?>
