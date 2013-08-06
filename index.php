3298fa687e4485c6516f600e5ff5cb8d7065589c
Ben52 <bygreenes@gmail.com>
2013-08-05 21:08:07 -0400

    add array examples

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
+   //an example of a regular array
+  $myarray = array('steve', 'joe', 'stan');
+  $myarray[] = 'asher';
 
-  print_r($_REQUEST);
+
+   print_r($myarray);
+
+// an example of an associative array
+   $associative_array = array('name1' => 'joe', 'name2' => 'stan');
+   $associative_array['myobject'] = new myclass();
+   print_r($associative_array);
+
+
+//an example of a count array function, it counts the values in the array
+   echo count($myarray);
 
 
 
-
+  class myclass{}
  ?>
