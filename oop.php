29ed1d75103416034cf74822fbed73e45634e21e
Ben52 <bygreenes@gmail.com>
2013-08-06 16:54:35 -0400

    added function to echo properties

--- oop.php
+++ oop.php
@@ -1,25 +1,36 @@
 <?php
      class cars {
            
-           public $doors;
+           public $size;
            public $color;
            public $year;
            public $name;
             
-          public function  __construct($doors, $color, $year, $name){
-          $this->doors = $doors;
-          $this->color = $color;
-          $this->year = $year;
-          $this->name = $name;
+          public function  __construct($a, $b, $c, $d){
+          $this->size = $a;
+          $this->color = $b;
+          $this->year = $c;
+          $this->name = $d;
           } 
+
+	public function st(){
+	echo	 $this->size . '<br/>' . $this->color . '<br/>' . $this->year . '<br/>' . $this->name;
+	}
+
+
      }
 
 
      $kiaOptima = new cars (4, 'tan', 2004, 'Kia Optima');
-    $fordExplorer = new cars (2, 'red', 2013, 'Ford Explorer');
-    
-print_r($kiaOptima);
-print_r($fordExplorer);
+     $fordExplorer = new cars (2, 'red', 2013, 'Ford Explorer');
+	     
+	 $kiaOptima->st();
+
+//	echo $kiaOptima->name . '<br/>';
+//	echo $kiaOptima->size;
+        
+//print_r($kiaOptima);
+//print_r($fordExplorer);
 
 
  ?>
