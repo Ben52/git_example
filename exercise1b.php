8ba614c6608264675957dca3078ce7d8be41010e
Ben52 <bygreenes@gmail.com>
2013-08-08 18:57:13 -0400

    first commit

---
+++ exercise1b.php
@@ -0,0 +1,69 @@
+<?php
+	//program to calculate debits and credits in an account
+
+  $obj = new account(1000);
+
+
+
+  $obj->debit(100, 'Walmart');
+  $obj->credit(200, 'cash deposit');
+  $obj->debit(500, 'Target');
+  $obj->debit(100, 'Sears');
+
+
+ // $transactions = $obj->debit(100);
+  print_r($obj);
+
+  class account{
+    
+    public $starting_balance;
+    public $current_balance;
+     
+    private $transactions = array(); 
+    
+    public function __construct($amount){
+      $this->starting_balance = $amount;
+      $this->current_balance = $amount;
+    }    
+
+    public function debit($amount, $source){
+      $transaction = array();
+      $transaction['type'] = 'debit';
+      $transaction['amount'] = $amount;
+      $transaction['source'] = $source;
+      $this->transactions[] = $transaction; 
+    //  $this->current_balance = $this->current_balance - $amount;
+        $this->transactions[]['debit'] = $amount;
+	//$this->transactions[]['source'] = $source;
+    }
+
+    public function credit($amount, $source){
+    //  $this->current_balance = $this->current_balance + $amount;
+        $this->transactions[]['credit'] = $amount;
+	//$this->transactions[]['source'] = $source;
+    }
+
+   public function process(){
+    foreach($this->transactions as $transaction);{
+      
+      print_r($transaction);
+    }
+      
+    
+   }
+
+
+
+  }
+
+
+  
+
+
+
+
+
+
+
+
+ ?>
