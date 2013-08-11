804e0175a0409168a55b3ca326f930cb9709e932
Ben52 <bygreenes@gmail.com>
2013-08-11 00:00:21 -0400

    practicing arrays

---
+++ arrays1.php
@@ -0,0 +1,25 @@
+<?php
+  $info = array('first_name' => '', 'middle_name' => '', 'last_name' => '');
+
+  $info['address'] = '85 Melville';
+  $info1 = array();
+
+  $info['first_name'] = 'Binyomin';
+  $info['last_name'] = 'Greenes';
+
+  $info1[] = $info;
+
+  $info['first_name'] = 'Esther';
+  $info['middle_name'] = '' ;
+  $info1[] = $info;
+ 
+ $info['first_name'] = 'Matisyahu';
+ $info['middle_name'] = 'Pinchos';
+  $info1[] = $info;
+
+print_r($info);
+print_r($info1);
+ 
+echo $info1[2][ 'last_name', 'first_name'];
+
+ ?>
