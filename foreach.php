89780f8b09d7425d8a91118e7512eb5de2f47ad2
Ben52 <bygreenes@gmail.com>
2013-08-17 23:33:04 -0400

    an array and a foreach

--- foreach.php
+++ foreach.php
@@ -1,14 +1,21 @@
 <?php 
   $a = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-  
+  $c[]  = $a;
   foreach($a as $b){
   echo $b . '<br>';
   }
 
+ if($a = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)){
+  echo '$a is an array';
+}else{
+  echo '$a is not an array';
+}
+
+print_r($c);
+$result = count($a);
+print_r($result);
+echo $c;
 
 
-
-
-
-
+    
 ?>
