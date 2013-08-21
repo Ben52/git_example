1084a43822ae10c93e0099c47e2d175e83b68590
Ben52 <bygreenes@gmail.com>
2013-08-21 00:48:29 -0400

    foreach loop

--- foreach.php
+++ foreach.php
@@ -1,6 +1,6 @@
 <?php 
   $a = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-  
+  $c[]  = $a;
   foreach($a as $b){
   echo $b . '<br>';
   }
@@ -12,13 +12,24 @@
 
 $a = 9;
 
+<<<<<<< HEAD
 $num = $a;
  
 
  
+=======
+ if($a = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)){
+  echo '$a is an array';
+}else{
+  echo '$a is not an array';
+}
+
+print_r($c);
+$result = count($a);
+print_r($result);
+echo $c;
+>>>>>>> 6ae6c49bbfa0790bef18d1fd9a2f7dcb405cd99c
 
 
-
-
-
+    
 ?>
