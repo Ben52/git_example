bdea1345749444617f5431cf7c60662849becd62
Ben52 <bygreenes@gmail.com>
2013-08-29 01:23:13 -0400

    does not add passwd conf to csv file

--- forms.php
+++ forms.php
@@ -46,9 +46,9 @@ $obj = new program();
 		 				'<FORM action="forms.php?class=login" method="post">
     		   				<P>
     		 					<LABEL for="username">Username: </LABEL>
-             						<INPUT type="text" name="username" id="username"><BR>
+             						<INPUT type="text" name="username" id="username" required="required"><BR>
 			 					<LABEL for="password">Password: </LABEL>
-			 						<INPUT type="password" name="password" id="password"><BR>
+			 						<INPUT type="password" name="password" id="password" required="required"><BR>
     		 					<INPUT type="submit" value="Send"> <INPUT type="reset">
     		 				</P>
 		 	 			</FORM>';
@@ -66,18 +66,36 @@ $obj = new program();
 		public $form = '<FORM action="forms.php?class=signup" method="post">
 						<P>
 							<LABEL for="firstname">First name: </LABEL>
-								<INPUT type="text" name="firstname" id="firstname"><BR>
+								<INPUT type="text" name="firstname" id="firstname" required="required"><BR>
 							<LABEL for="lastname">Last name: </LABEL>
-								<INPUT type="text" name="lastname" id="lastname"><BR>
+								<INPUT type="text" name="lastname" id="lastname" required="required"><BR>
 							<LABEL for="username">Choose a username: </LABEL>
-								<INPUT type="text" name="username" id="username"><BR>
+								<INPUT type="text" name="username" id="username" required="required"><BR>
 							<LABEL for="password">Choose a password: </LABEL>
-								<INPUT type="password" name="password" id="password"><BR>
+								<INPUT type="password" name="password" id="password" required="required"><BR>
 							<LABEL for="passwordconfirmation">Confirm your password: </LABEL>
-								<INPUT type="password" name="passwordconfirmation" id="passwordconfirmation"><BR>
+								<INPUT type="password" name="passwordconfirmation" id="passwordconfirmation" required="required"><BR>
 							<INPUT type="submit" value="Send"> <INPUT type="reset">
 						</P>
 						</FORM>';
+		
+		public function post(){
+			if($_POST['password'] == $_POST['passwordconfirmation']){
+				$userinfo = array();
+				$userinfo[]=$_POST['firstname'];
+				$userinfo[]=$_POST['lastname'];
+				$userinfo[]=$_POST['username'];
+				$userinfo[]=$_POST['password'];
+			$handle = fopen('users.csv', 'a+');
+			fputcsv($handle, $userinfo);
+			echo "Welcome, " . $_POST['firstname'] . " " . $_POST['lastname'] . "!";
+			}else{
+				//header('Location: ./forms.php?class=signup');
+				echo 'Sorry, your passwords don\'t match!' . $this->form;
+			}
+			
+		
+		}
 	}
 
 
