1600d6c36a2881746209700e8e8bd19d331fa3da
Ben52 <bygreenes@gmail.com>
2013-09-01 22:51:48 -0400

    added static menu

--- forms.php
+++ forms.php
@@ -12,6 +12,8 @@ $obj = new program();
 	}
 	
 	class page{
+		public static $menu = '<a href="forms.php?class=login">Login<br></a> <br> 
+					<a href="forms.php?class=signup">Signup</a>';
 		public function __construct(){
 			if($_SERVER['REQUEST_METHOD'] == 'GET'){
 				$this->get();
@@ -40,7 +42,7 @@ $obj = new program();
 	class homepage extends page{
 		public function get(){
 			echo '<body><h3>Welcome to the bank!</h3></body> <br>
-				 <a href="forms.php?class=login">Login</a> <br> 
+				 <a href="forms.php?class=login">Login<br></a> <br> 
 					<a href="forms.php?class=signup">Signup</a>';
 			
 		}
@@ -64,13 +66,11 @@ $obj = new program();
 		
 		public function post(){
 			$obj = new validation();
-			//echo 'Thank you for logging in, ' . "$_POST[username]" . "." . ' Have a wonderful day!<br><br><br>';
-			//echo '<a href="forms.php">Click here to retun to the Homepage</a>';
 		}
 	}
 	
 	class signup extends page{
-	
+		
 		
 		public 	$form = 
 					 '<FORM action="forms.php?class=signup" method="post">
@@ -96,23 +96,51 @@ $obj = new program();
 		public function post(){
 			$handle = fopen('users.csv', 'r+');
 			$record = fgetcsv($handle, 0, ',');
-			if($record[0]!=="First Name" && $record[1]!=="Last Name" && $record[2]!=="Username" && $record[3]!= "Password"){
-				fputcsv($handle, array('First Name', 'Last Name', 'Username', 'Password'));
+			if($record[0]!=="First Name" && $record[1]!=="Last Name" && $record[2]!=="Username" && $record[3]!= "Password" && $record[4] !== "Account Number"){
+				fputcsv($handle, array('First Name', 'Last Name', 'Username', 'Password', 'Account Number'));
 				print_r($record);
 				fclose($handle);
 			}
 			if($_POST['password'] !== $_POST['passwordconfirmation']){
 				throw new Exception('Passwords don\'t match');
 			}
-							$userinfo = array();
-							$userinfo[]=$_POST['firstname'];
-							$userinfo[]=$_POST['lastname'];
-							$userinfo[]=$_POST['username'];
-							$userinfo[]=$_POST['password'];
-						$handle = fopen('users.csv', 'a+');
-						fputcsv($handle, $userinfo);
-						echo "Welcome, " . $_POST['firstname'] . " " . $_POST['lastname'] . "!";
-						fclose($handle);
+			$row=1;
+			if(($handle = fopen('users.csv', 'r'))!==FALSE){
+				while(($record = fgetcsv($handle, 0, ','))!==FALSE){
+					if($row==1){
+						$keys=$record;
+						$row++;
+					}else{
+						$records[] = array_combine($keys, $record);
+					}
+				}
+				function makePrimaryKey($key_name, $records){
+					foreach($records as $record){
+						$index_name = $record[$key_name];
+						unset($record[$key_name]);
+						$new_records[$index_name] = $record;
+					}
+					return($new_records);
+				}
+				$sorted_records = makePrimaryKey('Username', $records);
+				fclose($handle);	
+			}
+			if(array_key_exists($_POST['username'], $sorted_records)){
+				echo "Sorry, that Username is already taken!<br>" . page::$menu;
+			}else{
+				$accountNum = rand(1000000000, 9999999999);
+				$userinfo = array();
+				$userinfo[]=$_POST['firstname'];
+				$userinfo[]=$_POST['lastname'];
+				$userinfo[]=$_POST['username'];
+				$userinfo[]=$_POST['password'];
+				$userinfo[]=$accountNum;
+				$handle = fopen('users.csv', 'a+');
+				fputcsv($handle, $userinfo);
+				echo "Welcome, " . $_POST['firstname'] . " " . $_POST['lastname'] . "! " . "Your account number is " . $accountNum . ".<br>" . page::$menu;
+				fclose($handle);
+			} 
+						    
 						
 		}
 	}
@@ -139,15 +167,17 @@ class validation{
 				return($new_records);
 			}
 			$sorted_records = makePrimaryKey('Username', $records);
+			fclose($handle);
 		}
 	$username_posted = $_POST['username'];
 	$password_posted = $_POST['password'];
 	if(array_key_exists($username_posted, $sorted_records) && $password_posted == $sorted_records[$username_posted]['Password']){
-		echo 'Welcome, ' . $_POST['username'];
-		
-	}else{
-		echo 'You were not found in our records';
-	}	
+		echo 'Welcome, ' . $sorted_records[$username_posted]['First Name'] . '!<br><br>' . page::$menu;
+		}elseif(!array_key_exists($username_posted, $sorted_records)){
+			echo 'We could not find you in our records. Sign up ' . '<a href="forms.php?class=signup">here.</a>';
+		}elseif($password_posted !== $sorted_records[$username_posted]['Password']){
+			echo 'Username and passord don\'t match. Please try again<br><br>' . '<a href="forms.php?class=login">Login</a>';
+		}	
 	}
 }
 	
